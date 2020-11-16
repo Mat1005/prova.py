a58ee31f2c9d965ebad76429a5ba7204d9417596
Matteo Zaccarelli <matteo10zac@gmail.com>
2020-11-16 17:27:43 +0100

    ho aggiunto degli spazi

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 print ( "ciao,questo progamma serve a calcolare la media dei voti." )
+
 materia  =  input ( "Dimmi di che materia stiamo parlando?" )
-
 sommatoria  =  0
-
 nvoti = int(input("quanti voti hai preso ?"))
+
+
 while  True:
 	
 	votocorrente = int(input( "inserisci un voto:"))
 	sommatoria += votocorrente 
+
 	if votocorrente == 0:
 		break
 
