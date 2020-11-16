83636901a0749a2acdec720ff4c85e65da8e876a
Matteo Zaccarelli <matteo10zac@gmail.com>
2020-11-16 17:29:47 +0100

    ho rinominato una variabile

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 print ( "ciao,questo progamma serve a calcolare la media dei voti." )
 
 materia  =  input ( "Dimmi di che materia stiamo parlando?" )
-sommatoria  =  0
+totale  =  0
 nvoti = int(input("quanti voti hai preso ?"))
 
 
 while  True:
 	
 	votocorrente = int(input( "inserisci un voto:"))
-	sommatoria += votocorrente 
+	totale += votocorrente 
 
 	if votocorrente == 0:
 		break
 
 
 
-media  =  sommatoria/nVoti
+media  =  totale/nVoti
 print ( 'Fino ad ora in' , materia , 'hai preso' , nVoti , 'voti la cui media è' , media )			
